2a8d9408756bf5d31109a985a73f47736846abf8
Sanjay_1 <59970250+Sanjay01112000@users.noreply.github.com>
2020-07-21 18:06:16 +0530

    two practice problems

---
+++ practice/Lucky_Four.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,count=0;
+        cin>>n;
+        while(n !=0)
+        {
+            if((n%10)==4)
+            {
+                count++;
+            }
+            n/=10;
+        }
+        cout<<count<<endl;
+    }
+}
