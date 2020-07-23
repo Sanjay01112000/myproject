cac00c61b8c5982c59f2be9954405a9f0e18fb8a
Sanjay_1 <59970250+Sanjay01112000@users.noreply.github.com>
2020-07-23 17:12:13 +0530

    practice program
    understaning behaviour of global vs local variable

---
+++ practice/RandomCode/GlobalVSLocalVariable.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int aglobal[3];
+int a=10;
+void Printing ( )
+{
+    cout<<a<<endl;
+
+    int a=5;
+    cout<<a<<endl;
+
+}
+int main()
+{
+    int alocal[3];
+    int a=5;
+    Printing();
+    cout<<"Algobal is: "<<aglobal[0]<<" "<<aglobal[1]<<" "<<aglobal[2]<<endl;
+    cout<<"Algobal is: "<<alocal[0]<<" "<<alocal[1]<<" "<<alocal[2]<<endl;
+    return 0;    
+}
